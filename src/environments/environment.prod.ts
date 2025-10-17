5f960ea353c7a717e96cb2b9ccdbb00407e59d8a
unknown <sebastian.vegar2015@gmail.com>
2025-10-17 12:34:04 -0500

    link para vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://triatlon-backend-fq83pji0k-johan-vegas-projects-1fad8aea.vercel.app'
+    apiUrl: 'https://triatlon-backend.vercel.app'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://triatlon-backend-fq83pji0k-johan-vegas-projects-1fad8aea.vercel.app'
+    apiUrl: 'https://triatlon-backend.vercel.app'
   };
